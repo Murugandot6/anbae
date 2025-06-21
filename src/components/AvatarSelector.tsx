d74aacf4c9632c8ed4c2ad310e2e20a1fb03352e
[dyad] <mskdot6@gmail.com>
2025-06-21 19:23:42 +0530

    [dyad] Added a red border to avatars in `AvatarSelector` for debugging and provided detailed instructions for the user to inspect the browser's Network tab and element styles. - wrote 1 file(s)

--- src/components/AvatarSelector.tsx
+++ src/components/AvatarSelector.tsx
@@ -31,7 +31,7 @@ const AvatarSelector: React.FC<AvatarSelectorProps> = ({ selectedAvatar, onSelec
             )}
             onClick={() => onSelect(path)}
           >
-            <Avatar className="w-14 h-14 sm:w-16 sm:h-16 md:w-18 md:h-18 lg:w-20 lg:h-20 aspect-square rounded-full overflow-hidden"> {/* Added aspect-square and explicit rounded-full/overflow-hidden */}
+            <Avatar className="w-14 h-14 sm:w-16 sm:h-16 md:w-18 md:h-18 lg:w-20 lg:h-20 aspect-square rounded-full overflow-hidden block flex-shrink-0 border-2 border-red-500"> {/* Added aspect-square, explicit rounded-full/overflow-hidden, block, flex-shrink-0, and red border for debugging */}
               <AvatarImage src={path} alt={`Avatar ${path.split('/').pop()?.split('.')[0]}`} className="object-cover" />
               <AvatarFallback className="bg-gray-300 dark:bg-gray-600 text-gray-700 dark:text-gray-300">
                 AV
