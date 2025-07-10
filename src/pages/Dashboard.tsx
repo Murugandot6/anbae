f3fb2896916fff5a264db5183ddfc733f89526b4
[dyad] <mskdot6@gmail.com>
2025-07-10 22:43:42 +0530

    [dyad] Added quick action icons to dashboard - wrote 2 file(s)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -22,6 +22,7 @@ import JournalEntryCard from '@/components/JournalEntryCard'; // Import the rena
 import { Helmet } from 'react-helmet-async'; // Import Helmet
 import LoadingPulsar from '@/components/LoadingPulsar';
 import { useIsMobile } from '@/hooks/use-mobile';
+import QuickActions from '@/components/QuickActions'; // Import QuickActions
 
 const Dashboard = () => {
   const { user, loading: sessionLoading } = useSession();
@@ -333,6 +334,7 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
+        <QuickActions /> {/* Add the QuickActions component here */}
       </BackgroundWrapper>
     </>
   );
