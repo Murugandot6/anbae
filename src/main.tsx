17876fff83bcce5f9df62578b518b832a038b960
[dyad] <mskdot6@gmail.com>
2025-07-11 00:24:49 +0530

    [dyad] Wrapped the App component with HelmetProvider to fix the react-helmet-async error. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import './globals.css'; // Corrected import path
 import { ThemeProvider } from './components/theme-provider.tsx';
 import { Toaster } from 'sonner';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { HelmetProvider } from 'react-helmet-async'; // Import HelmetProvider
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap App with BrowserRouter */}
+    <BrowserRouter>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <App />
+        <HelmetProvider> {/* Wrap App with HelmetProvider */}
+          <App />
+        </HelmetProvider>
         <Toaster />
       </ThemeProvider>
     </BrowserRouter>
